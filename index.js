1dec9335346cb4d803cf88631d4769cb4322833a
Paul <pki@midmaine.com>
2018-03-26 17:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromObjectByKey(recipes, ingredient) {
   return pie
 }
 
-function destructivelyDeleteFromObjectbyKey(recipes, ingredient) {
+function destructivelyDeleteFromObjectByKey(recipes, ingredient) {
   delete recipes[ingredient]
   return recipes
 }
